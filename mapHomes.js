31d183aebfe903bf0e6bcb09363706791fe9b654
AlexPescud <ap980@uowmail.edu.au>
2019-08-18 15:23:05 +1000

    a

--- mapHomes.js
+++ mapHomes.js
@@ -1,9 +1,23 @@
+<!DOCTYPE HTML>
+<html lang = "en">
+<head>
+  <!-- basic.html -->
+  <title>basic</title>
+  <meta charset = "UTF-8" />
+</head>
+<body>
+  <script src="/d3.min.js"></script>
 
+  <h1>Level One Headline</h1>
+  <p>
+    This is a paragraph.
+    Note that the text is automatically wrapped.
+  </p>
   <script>
       var w = "960"
       var h = "600"
 
-      console.log("attempt to set up svg");
+      //console.log("attempt to set up svg");
 
       var svg = d3.select("body")
             .append("svg")
@@ -18,11 +32,19 @@
         //.style("fill", "black");
 
     </script>
+</body>
+</html>
 
-<!DOCTYPE HTML>
+  <!doctype html>
+<html lang="en">
 <html>
-<meta charset="utf-8"/>
   <head>
-    <title>Live Art Week UOW</title>
-    <script type="text/javascript" src="d3.min.js"></script>
+    <meta charset="utf-8">
+    <title>My first map in D3</title> 
   </head>
+  <body>
+    <H1>Example</H1>
+    <script src="//d3js.org/d3.v4.min.js"></script> 
+    <script> your code goes here </script>
+  </body>
+</html>
